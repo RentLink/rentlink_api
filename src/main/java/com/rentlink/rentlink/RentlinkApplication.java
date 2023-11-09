eb00ef438d1458b1435297d34ef397ac9950a5b1
szasiii <konradszatan11@gmail.com>
2023-11-09 13:55:55 +0100

    Init project

---
+++ src/main/java/com/rentlink/rentlink/RentlinkApplication.java
@@ -0,0 +1,13 @@
+package com.rentlink.rentlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentlinkApplication.class, args);
+	}
+
+}
