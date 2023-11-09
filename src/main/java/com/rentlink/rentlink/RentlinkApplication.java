6b7564cbf2d58fcd91b068cc1feb6b14068ff126
szasiii <konradszatan11@gmail.com>
2023-11-09 14:04:00 +0100

    Dummy endpoint

--- src/main/java/com/rentlink/rentlink/RentlinkApplication.java
+++ src/main/java/com/rentlink/rentlink/RentlinkApplication.java
@@ -2,12 +2,20 @@ package com.rentlink.rentlink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class RentlinkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RentlinkApplication.class, args);
+	}
+
+	@GetMapping("/test")
+	public String test() {
+		return "SUCCESS";
 	}
 
 }
