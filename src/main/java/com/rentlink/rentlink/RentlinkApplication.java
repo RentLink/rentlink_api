f35bf330d38f61a46106bb21ba901fb83ce19a0c
Konrad <konradszatan11@gmail.com>
2024-01-04 15:53:56 +0100

    CORS pt2 (#8)

--- src/main/java/com/rentlink/rentlink/RentlinkApplication.java
+++ src/main/java/com/rentlink/rentlink/RentlinkApplication.java
@@ -2,6 +2,7 @@ package com.rentlink.rentlink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class RentlinkApplication {
--- src/main/java/com/rentlink/rentlink/manage_unit_data/UnitEndpoint.java
+++ src/main/java/com/rentlink/rentlink/manage_unit_data/UnitEndpoint.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/unit")
 @RequiredArgsConstructor
+@CrossOrigin
 class UnitEndpoint {
 
     private final UnitExternalAPI unitExternalAPI;
