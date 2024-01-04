687d337013299d9bf7e787cac89616266ead23a3
szasiii <konradszatan11@gmail.com>
2024-01-04 15:21:25 +0100

    CORS

---
+++ src/main/java/com/rentlink/rentlink/WebConfig.java
@@ -0,0 +1,21 @@
+package com.rentlink.rentlink;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("POST", "GET", "PUT", "PATCH", "DELETE")
+                .allowedHeaders("Content-Type")
+                .exposedHeaders("Content-Type")
+                .allowCredentials(true);
+    }
+}
