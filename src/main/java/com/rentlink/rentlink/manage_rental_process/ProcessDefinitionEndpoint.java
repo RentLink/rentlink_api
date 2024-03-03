1cf5f70294898459682ade98a4ffe68a8215cf44
szasiii <konradszatan11@gmail.com>
2024-03-03 22:07:04 +0100

    Process def + account

--- src/main/java/com/rentlink/rentlink/manage_rental_process/ProcessDefinitionEndpoint.java
+++ src/main/java/com/rentlink/rentlink/manage_rental_process/ProcessDefinitionEndpoint.java
@@ -1,15 +1,14 @@
 package com.rentlink.rentlink.manage_rental_process;
 
+import static com.rentlink.rentlink.common.CustomHeaders.X_USER_HEADER;
+
 import java.util.List;
 import java.util.UUID;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static com.rentlink.rentlink.common.CustomHeaders.X_USER_HEADER;
 
 @RestController
 @RequestMapping("/api/process-definitions")
