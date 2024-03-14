c5ad5307ed0738754676f3410a0ca0bbe4634b6a
Konrad <konradszatan11@gmail.com>
2024-03-14 01:03:26 +0100

    WS allow origin (#36)

--- src/main/java/com/rentlink/rentlink/manage_notifications/WebSocketConfig.java
+++ src/main/java/com/rentlink/rentlink/manage_notifications/WebSocketConfig.java
@@ -15,6 +15,6 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(notificationsWebSocketHandler, "/ws/notifications");
+        registry.addHandler(notificationsWebSocketHandler, "/ws/notifications").setAllowedOrigins("*");
     }
 }
